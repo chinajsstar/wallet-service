4311cf7515f3278ae27f054463da6ec910690e7b
刘旭良 <liuxuliang@yeah.net>
2018-04-19 12:59:01 +0800

    增加地址功能过滤

--- src/business_center/business/business_test.go
+++ src/business_center/business/business_test.go
@@ -20,14 +20,16 @@ func TestHandleMsg(t *testing.T) {
 	switch testType {
 	case 1:
 		req.Data.Method.Function = "new_address"
-		req.Data.Argv.LicenseKey = "737205c4-af3c-426d-973d-165a0bf46c71"
+		req.Data.Argv.UserKey = "737205c4-af3c-426d-973d-165a0bf46c71"
 		req.Data.Argv.Message = "{\"id\":\"1\",\"symbol\":\"eth\",\"count\":1}"
 	case 2:
 		req.Data.Method.Function = "withdrawal"
-		req.Data.Argv.LicenseKey = "737205c4-af3c-426d-973d-165a0bf46c71"
+		req.Data.Argv.UserKey = "737205c4-af3c-426d-973d-165a0bf46c71"
 		req.Data.Argv.Message = "{\"user_order_id\":\"1\",\"symbol\":\"eth\",\"amount\":0.1,\"to_address\":\"0x00000\",\"user_timestamp\":0}"
 	case 3:
 		req.Data.Method.Function = "query_user_address"
+		req.Data.Argv.UserKey = "737205c4-af3c-426d-973d-165a0bf46c71"
+		req.Data.Argv.Message = "{\"page_index\":1,\"max_display\":1,\"create_time_begin\":1523656800}"
 	}
 
 	if testType > 0 {
