cce28acc7cfe276814a4f4a6f0486cb0893f90f3
刘旭良 <liuxuliang@yeah.net>
2018-04-26 16:24:25 +0800

    修改部分bug

--- src/business_center/business/business_test.go
+++ src/business_center/business/business_test.go
@@ -14,15 +14,15 @@ func TestHandleMsg(t *testing.T) {
 	var req data.SrvRequestData
 	var res data.SrvResponseData
 
-	testType := 0
+	testType := 2
 	switch testType {
 	case 1:
 		req.Data.Method.Function = "new_address"
-		req.Data.Argv.UserKey = "6cc6f813-c031-40f3-8e3a-9332f5f67a93"
+		req.Data.Argv.UserKey = "32fe3bf7-8154-4b6f-845c-dc8414c81d21"
 		req.Data.Argv.Message = "{\"id\":\"1\",\"symbol\":\"eth\",\"count\":1}"
 	case 2:
 		req.Data.Method.Function = "withdrawal"
-		req.Data.Argv.UserKey = "f60a9a51-9f4d-4c17-8041-6defcff6a4cc"
+		req.Data.Argv.UserKey = "32fe3bf7-8154-4b6f-845c-dc8414c81d21"
 		req.Data.Argv.Message = "{\"user_order_id\":\"1\",\"symbol\":\"eth\",\"amount\":0.1,\"to_address\":\"0x357859b176a72f3167e867b1cf0c1e04abba1ce1\",\"user_timestamp\":0}"
 	case 3:
 		req.Data.Method.Function = "query_user_address"
