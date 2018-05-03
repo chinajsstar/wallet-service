3ab02e61ee19a1fb58308b0b26475f0964339d32
henly.liu <henly.liu@blockshine.com>
2018-05-03 09:50:05 +0800

    修改makefile和gitmodule的错误

--- src/api_router/base/utils/text.go
+++ src/api_router/base/utils/text.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"crypto/md5"
 	"encoding/hex"
+	"os"
+	"io"
 )
 
 // Scan input to string with '\n' with end line
@@ -28,4 +30,23 @@ func GetMd5Text(text string) string {
 	sum := h.Sum(nil)
 
 	return hex.EncodeToString(sum)
+}
+
+func CopyFile(src, dst string)(w int64, err error){
+	srcFile,err := os.Open(src)
+	if err!=nil{
+		fmt.Println(err.Error())
+		return
+	}
+	defer srcFile.Close()
+
+	dstFile,err := os.Create(dst)
+	if err!=nil{
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer dstFile.Close()
+
+	return io.Copy(dstFile,srcFile)
 }
