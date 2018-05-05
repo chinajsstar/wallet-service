86f42ebec1cfd8bfba0ede11e758040f17d6d488
liuxuliang <liuxuliang@yeah.net>
2018-05-05 20:15:08 +0800

    修改接口文档

--- src/business_center/address/assistor.go
+++ src/business_center/address/assistor.go
@@ -584,3 +584,42 @@ func unpackJson(s string) ParamsMapping {
 	}
 	return params
 }
+
+func unpackJson2(s string, userClass int) string {
+	var v interface{}
+	err := json.Unmarshal([]byte(s), &v)
+	if err != nil {
+		return ""
+	}
+
+	switch reflect.TypeOf(v).Kind() {
+	case reflect.Slice:
+		resArr := make([]interface{}, 0)
+		if jsonArr, ok := v.([]interface{}); ok {
+			for _, value := range jsonArr {
+				resArr = append(resArr, value)
+			}
+		}
+	case reflect.Map:
+		resMap := make(map[string]interface{})
+		if jsonMap, ok := v.(map[string]interface{}); ok {
+			for key, value := range jsonMap {
+				switch key {
+				case "user_key":
+					if userClass == 1 {
+						resMap[key] = value
+					}
+				case "asset_name":
+					resMap[key] = value
+				case "address":
+					resMap[key] = value
+				case "amount":
+					resMap[key] = value
+				case "count":
+					resMap[key] = value
+				}
+			}
+		}
+	}
+	return ""
+}
