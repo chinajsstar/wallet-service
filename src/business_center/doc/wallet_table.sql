9b033e4455916944c266c26c3619162f998005b1
liuxuliang <liuxuliang@yeah.net>
2018-05-17 10:30:36 +0800

    数据库增加user_order_id

--- src/business_center/doc/wallet_table.sql
+++ src/business_center/doc/wallet_table.sql
@@ -231,6 +231,7 @@ DROP TABLE IF EXISTS `withdrawal_order`;
 CREATE TABLE `withdrawal_order` (
   `id` int(11) NOT NULL AUTO_INCREMENT,  
   `order_id` varchar(255) NOT NULL DEFAULT '',
+  `user_order_id` varchar(255) NOT NULL DEFAULT '',
   `user_key` varchar(255) NOT NULL DEFAULT '',
   `asset_name` varchar(255) NOT NULL DEFAULT '',
   `address` varchar(255) NOT NULL DEFAULT '',
