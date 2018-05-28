e7d1f22211d36808f2e74b138e3a4df8dd872f29
liuxuliang <liuxuliang@yeah.net>
2018-05-28 11:23:26 +0800

    增加交易接口

--- src/bastionpay_api/api/v1/bastionpay.go
+++ src/bastionpay_api/api/v1/bastionpay.go
@@ -255,9 +255,9 @@ type (
 	}
 
 	ReqPostTransaction struct {
-		AssetName string
-		From      string
-		To        string
-		Amount    float64
+		AssetName string  `json:"asset_name"`
+		From      string  `json:"from"`
+		To        string  `json:"to"`
+		Amount    float64 `json:"amount"`
 	}
 )
